077fe1a83475ee93e1bb390d9646412ff2971784
Hack Reactor Student <student@hackreactor.com>
2018-07-09 14:36:33 -0700

    Add functionalities

--- app.js
+++ app.js
@@ -1,26 +1,76 @@
 document.addEventListener("DOMContentLoaded", (event) =>{
   
-  var boardEl = document.getElementById('board');
-  
-  
-  var board = [['_','_','_'],['_','_','_'],['_','_','_']];
-  
+  var board = [['','',''],['','',''],['','','']];
   var selectedSquare;
-  
+  var currentSign = 'X';
+  var gameover = false;  
+  reset = () => {
+    window.location.reload();
+  }
   handleClick = (e) =>{
-    var squareId = e.srcElement.id;
-    changeSignInSquare(squareId);
+    if(!gameover){
+      var squareId = e.srcElement.id;
+      changeSignInSquare(squareId);
+      checkBoardForWin(currentSign);
+    }
   };
   
   changeSignInSquare = (id) => {
     var row = parseInt(id[0]);
     var col = parseInt(id[1]);
 
-    if (board[row][col] === '_') {
-      board[row][col] = 'X';
+    if (!board[row][col]){
+      board[row][col] = currentSign;
+      renderSign (id, currentSign);
     } else {
-      board[row][col] = '_';
+      return alert("Invalid Move. Try again!");
     }
-    console.log(board[row][col]);
-  }
+  };
+  
+  renderSign = (id, sign) => {
+    document.getElementById(id).innerText = sign;
+  };
+  
+  checkBoardForWin = (sign) => {
+    if (find3inRow(sign) || find3inColumn(sign) || find3inDiagonal(sign)) {
+      handleEndGame (currentSign);
+    } else {
+      currentSign = currentSign === 'X'? 'O' : 'X';
+    }
+    
+  };
+  
+  find3inRow = (sign) => {
+    return board.some((row) => {
+      return row.every ((square) => {
+        return square === sign;
+      });
+    });
+  };
+  
+  find3inColumn = (sign) => {
+    return board[0].some((val, index) => {
+      return val === sign && board[1][index] === sign && board[2][index] === sign; 
+    });
+  };
+  
+  find3inDiagonal = (sign) => {
+    var majorDiagonalWin = board.every((row, index) => {
+      return row[index] === sign;
+    });
+    
+    var minorDiagonalWin = board.every((row, index) => {
+      return row[2-index] === sign;
+    });
+    
+    return majorDiagonalWin || minorDiagonalWin;
+  };
+  
+  handleEndGame = (sign) => {
+    document.getElementById('message').innerText = sign + ' WINS!';
+    gameover = true;
+  };
+  
+  
+  
 });
