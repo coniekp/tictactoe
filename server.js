6bf4d55316d87815676190da7229180ac13e5818
Hack Reactor Student <student@hackreactor.com>
2018-07-09 19:57:23 -0700

    Initialize server

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var server = express();
+
+server.listen(3000, () => console.log('Listening locally'));
+
+app.post('/player1', ()=>{});
+app.post('/player2', () =>{});
